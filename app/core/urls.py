2e09cdd84a58f323042b58d9b561a63732844197
oyekamal <autocratickamal1@gmail.com>
2024-03-04 14:55:47 +0500

    all auth api added

--- app/core/urls.py
+++ app/core/urls.py
@@ -19,12 +19,42 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Stream ad api",
+        default_version="v1",
+        description="Find-need backend in django.",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("advertisement.urls")),
     path("accounts/", include("allauth.urls")),
+    path("api/account/", include("dj_rest_auth.urls")),
+    path('api/registration/', include('dj_rest_auth.registration.urls')),
+    path("api/auth/", include("authentication.urls")),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 

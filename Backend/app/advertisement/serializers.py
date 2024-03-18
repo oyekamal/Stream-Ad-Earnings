dc72f78288f23ee55608ab1a8bc123f4b20af578
oyekamal <autocratickamal1@gmail.com>
2024-03-18 12:47:18 +0500

    react docker configuration added

--- Backend/app/advertisement/serializers.py
+++ Backend/app/advertisement/serializers.py
@@ -1,21 +1,23 @@
 from rest_framework import serializers
 from .models import Advertisement, AdvertisementGroup
 
-
+# Serializer for the Advertisement model
 class AdvertisementSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Advertisement
-        fields = "__all__"
+        model = Advertisement  # Specifies the model to serialize
+        fields = "__all__"  # Includes all fields from the Advertisement model in the serialization
 
-
+# Serializer for the AdvertisementGroup model
 class AdvertisementGroupSerializer(serializers.ModelSerializer):
+    # Custom field to include related advertisements
     advertisement = serializers.SerializerMethodField()
 
+    # Method to get advertisements related to a specific AdvertisementGroup instance
     def get_advertisement(self, obj):
-        ads = Advertisement.objects.filter(group=obj)
-        serializer = AdvertisementSerializer(ads, many=True)
-        return serializer.data
+        ads = Advertisement.objects.filter(group=obj)  # Query to get all advertisements belonging to the group
+        serializer = AdvertisementSerializer(ads, many=True)  # Serialize the list of advertisements
+        return serializer.data  # Return the serialized data
 
     class Meta:
-        model = AdvertisementGroup
-        fields = "__all__"
+        model = AdvertisementGroup  # Specifies the model to serialize
+        fields = "__all__"  # Includes all fields from the AdvertisementGroup model in the serialization

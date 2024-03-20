8a5614e6a8aa82becb8c008205f1ff608f459a69
oyekamal <autocratickamal1@gmail.com>
2024-03-20 10:02:04 +0500

    api added

--- Frontend/src/redux/Slice/AdSlice/saga.ts
+++ Frontend/src/redux/Slice/AdSlice/saga.ts
@@ -23,6 +23,74 @@ export function* getAdsGroups({ payload }: any): any {
     }
 }
 
+
+export function* postAdsGroups({ token, payload }: any): any {
+
+    const requestUrl = `http://localhost:8000/api/ads_group/`;
+    try {
+        const data = yield call(() =>
+            fetch(requestUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    'Authorization': `token ${token}`,
+                },
+                body: JSON.stringify(payload),
+            })
+        );
+        const parsedData = yield data.json();
+        // yield put(AdsGroupsSuccess(parsedData));
+        message.success("Ads groups successfully added");
+    } catch (err) {
+        // yield put(AdsGroupsFailed());
+    }
+}
+
+
+export function* putAdsGroups({ token, payload }: any): any {
+
+    const requestUrl = `http://localhost:8000/api/ads_group/${payload.id}`;
+    try {
+        const data = yield call(() =>
+            fetch(requestUrl, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    'Authorization': `token ${token}`,
+                },
+                body: JSON.stringify(payload),
+            })
+        );
+        const parsedData = yield data.json();
+        // yield put(AdsGroupsSuccess(parsedData));
+        message.success("Ads groups successfully Edit");
+    } catch (err) {
+        // yield put(AdsGroupsFailed());
+    }
+}
+
+export function* deleteAdsGroups({ token, id }: any): any {
+
+    const requestUrl = `http://localhost:8000/api/ads_group/${id}`;
+    try {
+        const data = yield call(() =>
+            fetch(requestUrl, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    'Authorization': `token ${token}`,
+                },
+                // body: JSON.stringify(payload),
+            })
+        );
+        const parsedData = yield data.json();
+        // yield put(AdsGroupsSuccess(parsedData));
+        message.success("Ads groups successfully Deleted");
+    } catch (err) {
+        // yield put(AdsGroupsFailed());
+    }
+}
+
 function* adSaga() {
     yield takeEvery("AdsGroups/GetAdsGroups", getAdsGroups);
 }
